87d599747e8e74db0bf16660df112589e8e0072a
Michael Parker <parker_michael@me.com>
2020-02-22 17:11:33 +0000

    some minor formatting;

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,26 +5,27 @@ import { graphql } from "gatsby"
 // import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
 
 const Venue = ({ data }) => {
   let label = "";
-  
+  let districts = []
   if(data.District.length && data.District[0].data.Name){
-
-    label = <label>{data.District[0].data.Name}</label>
+    data.District.forEach(district => districts.push(district.data.Name))
+    label = <p style={{fontSize: '0.8rem', marginBottom: '4px'}}>{districts.join("/")}</p>
   }
-  console.log(data)
+
   return (
-    <div>
+    <div key={data} style={{ marginBottom: "24px" }}>
       {label}
-      <h4>{data.Name}</h4>
+      <h3 style={{ marginTop: "4px" }}>{data.Name}</h3>
     </div>
   )
 }
 
 const VenueList = ({ venues }) => {
   let venueList = venues.map((venue) => {
-    return <Venue data={venue.node.data} />
+    return <Venue data={venue.node.data} key={venue.node.id}/>
   })
   return(
   <div>
@@ -34,31 +35,34 @@ const VenueList = ({ venues }) => {
 }
 
 const IndexPage = ({ data }) => (
-  <div style={{ margin: "auto", width: "960px" }}>
+  <Layout>
+    <br />
     <SEO title="Home" />
     <h1> Best Venues of London</h1>
     <p>A guide to restaurants, bars, cafes and clubs</p>
+    <hr />
     <VenueList venues={data.allAirtable.edges} />
-  </div>
+  </Layout>
 )
 
 export default IndexPage
 
 export const query = graphql`
-  query {
-    allAirtable(filter: { table: { eq: "Venues" } }) {
-      edges {
-        node {
-          data {
-            Name
-            District {
-              data {
-                Name
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+         query {
+           allAirtable(filter: { table: { eq: "Venues" } }) {
+             edges {
+               node {
+                 id
+                 data {
+                   Name
+                   District {
+                     data {
+                       Name
+                     }
+                   }
+                 }
+               }
+             }
+           }
+         }
+       `
